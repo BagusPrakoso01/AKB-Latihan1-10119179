c1c7da07761647d778ca48d3a20cb3fb7799e82f
BagusP <bagus.10119179@mahasiswa.unikom.ac.id>
2022-04-19 19:32:43 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a10119179latihan1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.a10119179latihan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+//18-04-22_10119179_MuhamadBagusPrakoso_IF5
---
+++ app/src/test/java/com/example/a10119179latihan1/ExampleUnitTest.java
@@ -0,0 +1,18 @@
+package com.example.a10119179latihan1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
+//18-04-22_10119179_MuhamadBagusPrakoso_IF5
